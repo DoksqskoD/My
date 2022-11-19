a35e91eab04348192c07df6bd5dc075624ea3fef
Doksq <Doksqdoksq@gmail.com>
2022-11-19 21:37:55 +0300

    19.11.2022\5

--- Test/Test1.py
+++ Test/Test1.py
@@ -1,7 +1,3 @@
-import numpy as np
-
-new_arr = np.array([16, 20, 12, 10, 8, 22, 97, 75, 43])
-print("Creation of array:",new_arr)
-final_output = np.fromiter((i for i in new_arr if i < 25), dtype = new_arr.dtype)
-
-print("Filtering array:",final_output)
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+PP=[elem for elem in a if elem < 5]
+print([elem-1 for elem in a if elem < 5])
