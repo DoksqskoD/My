678e3f12e461d5ba989e874fc32cdd32721ef342
Doksq <Doksqdoksq@gmail.com>
2022-11-19 11:47:08 +0300

    19.11.2022\2

--- Test/Test1.py
+++ Test/Test1.py
@@ -1,5 +1,7 @@
+import numpy as np
 
-import os
-_PID = ('tasklist /FO CSV').split('\n')
-result = os.popen(_PID[0]).read()
-print(result)
+new_arr = np.array([16, 20, 12, 10, 8, 22, 97, 75, 43])
+print("Creation of array:",new_arr)
+final_output = np.fromiter((i for i in new_arr if i < 25), dtype = new_arr.dtype)
+
+print("Filtering array:",final_output)
