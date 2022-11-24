a4db2feff14a923917a6a8a72a9db8709a3b8508
Doksq <Doksqdoksq@gmail.com>
2022-11-24 20:40:47 +0300

    24.11.2022\1

--- Test/Test1.py
+++ Test/Test1.py
@@ -1,1 +1,18 @@
+import numpy
 
+class MyClass:
+
+    def __init__(self, Namber):
+        Array=[]
+
+    def Tunning(self):
+        self.Array=numpy.array(self.Array)
+
+A=MyClass()
+b=MyClass()
+C=MyClass()
+D=MyClass()
+
+b.Tunning()
+print(type(A),type(b),type(C),type(D))
+print(type(A.Array),type(b.Array),type(C.Array),type(D.Array))
